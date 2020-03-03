13322c5022c779327a0d73f4c5cf2436cdfa6b9e
Cyanox <Infernogaming621@gmail.com>
2020-03-02 19:08:06 -0500

    Add project files.

---
+++ DCReplace/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using EXILED;
+
+namespace DCReplace
+{
+	public class Plugin : EXILED.Plugin
+	{
+		private EventHandlers ev;
+
+		public override void OnEnable()
+		{
+			ev = new EventHandlers();
+
+			Events.PlayerLeaveEvent += ev.OnPlayerLeave;
+		}
+
+		public override void OnDisable()
+		{
+			ev = null;
+		}
+
+		public override void OnReload() { }
+
+		public override string getName { get; } = "DCReplace";
+	}
+}
