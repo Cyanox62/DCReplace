bf2909996f16b08f2c01288ae61ec6d12a59b25b
Cyanox <Infernogaming621@gmail.com>
2020-03-03 20:13:32 -0500

    Added a check to make sure the player who disconnected is alive because i forgot it like a pepega

--- DCReplace/EventHandlers.cs
+++ DCReplace/EventHandlers.cs
@@ -12,50 +12,53 @@ namespace DCReplace
 	{
 		public void OnPlayerLeave(PlayerLeaveEvent ev)
 		{
-			bool is035 = false;
-			bool isSH = false;
-			try
+			if (ev.Player.GetTeam() != Team.RIP)
 			{
-				is035 = ev.Player.queryProcessor.PlayerId == Scp035Data.GetScp035()?.queryProcessor.PlayerId;
-			}
-			catch (Exception x)
-			{
-				Log.Warn("SCP-035 is not installed, skipping method call...");
-			}
-
-			try
-			{
-				isSH = SerpentsHand.API.SerpentsHand.GetSHPlayers().Contains(ev.Player.queryProcessor.PlayerId);
-			}
-			catch (Exception x)
-			{
-				Log.Warn("SerpentsHand is not installed, skipping method call...");
-			}
-
-			Inventory.SyncListItemInfo items = ev.Player.inventory.items;
-			RoleType role = ev.Player.GetRole();
-			Vector3 pos = ev.Player.transform.position;
-			int health = (int)ev.Player.playerStats.health;
-			string ammo = ev.Player.ammoBox.amount;
-
-			ReferenceHub player = Player.GetHubs().FirstOrDefault(x => x.GetRole() == RoleType.Spectator && x.characterClassManager.UserId != string.Empty && !x.GetOverwatch());
-			if (player != null)
-			{
-				if (isSH) SerpentsHand.API.SerpentsHand.SpawnPlayer(player, false);
-				else player.SetRole(role);
-				if (is035) Scp035Data.Spawn035(player);
-				Timing.CallDelayed(0.3f, () =>
+				bool is035 = false;
+				bool isSH = false;
+				try
 				{
-					player.SetPosition(pos);
-					player.inventory.items.ToList().Clear();
-					foreach (var item in items)
+					is035 = ev.Player.queryProcessor.PlayerId == Scp035Data.GetScp035()?.queryProcessor.PlayerId;
+				}
+				catch (Exception x)
+				{
+					Log.Warn("SCP-035 is not installed, skipping method call...");
+				}
+
+				try
+				{
+					isSH = SerpentsHand.API.SerpentsHand.GetSHPlayers().Contains(ev.Player.queryProcessor.PlayerId);
+				}
+				catch (Exception x)
+				{
+					Log.Warn("SerpentsHand is not installed, skipping method call...");
+				}
+
+				Inventory.SyncListItemInfo items = ev.Player.inventory.items;
+				RoleType role = ev.Player.GetRole();
+				Vector3 pos = ev.Player.transform.position;
+				int health = (int)ev.Player.playerStats.health;
+				string ammo = ev.Player.ammoBox.amount;
+
+				ReferenceHub player = Player.GetHubs().FirstOrDefault(x => x.GetRole() == RoleType.Spectator && x.characterClassManager.UserId != string.Empty && !x.GetOverwatch());
+				if (player != null)
+				{
+					if (isSH) SerpentsHand.API.SerpentsHand.SpawnPlayer(player, false);
+					else player.SetRole(role);
+					if (is035) Scp035Data.Spawn035(player);
+					Timing.CallDelayed(0.3f, () =>
 					{
-						player.inventory.AddNewItem(item.id);
-					}
-					player.playerStats.health = health;
-					player.ammoBox.Networkamount = ammo;
-					player.Broadcast(5, "<i>You have replaced a player who has disconnected.</i>", false);
-				});
+						player.SetPosition(pos);
+						player.inventory.items.ToList().Clear();
+						foreach (var item in items)
+						{
+							player.inventory.AddNewItem(item.id);
+						}
+						player.playerStats.health = health;
+						player.ammoBox.Networkamount = ammo;
+						player.Broadcast(5, "<i>You have replaced a player who has disconnected.</i>", false);
+					});
+				}
 			}
 		}
 	}
