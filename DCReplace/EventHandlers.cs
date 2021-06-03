9b316c5463dbe44647530bd7a4b9f216c4713a6c
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-06-02 21:29:59 -0700

    add support for latest SH and 035 + cleanup

--- DCReplace/EventHandlers.cs
+++ DCReplace/EventHandlers.cs
@@ -1,10 +1,6 @@
 ﻿using MEC;
 using System.Linq;
-using UnityEngine;
-using scp035.API;
 using System;
-using CISpy.API;
-using System.Collections.Generic;
 using Exiled.Events.EventArgs;
 using Exiled.API.Features;
 using Exiled.API.Enums;
@@ -14,43 +10,31 @@ namespace DCReplace
 {
 	class EventHandlers
 	{
-		private bool isContain106;
-		private bool isRoundStarted = false;
-
-		private Player TryGet035() => Scp035Data.GetScp035();
-
-		private List<Player> TryGetSH() => SerpentsHand.API.SerpentsHand.GetSHPlayers();
-
-		private Dictionary<Player, bool> TryGetSpies() => SpyData.GetSpies();
-
-		private void TrySpawnSpy(Player player, Player dc, Dictionary<Player, bool> spies) => SpyData.MakeSpy(player, spies[dc], false);
-
-		private void TrySpawnSH(Player player) => SerpentsHand.API.SerpentsHand.SpawnPlayer(player, false);
-
-		private void TrySpawn035(Player player) => Scp035Data.Spawn035(player);
+		private bool _isContain106;
+		private bool _isRoundStarted;
 
 		public void OnRoundStart()
 		{
-			isContain106 = false;
-			isRoundStarted = true;
+			_isContain106 = false;
+			_isRoundStarted = true;
 		}
 
-		public void OnRoundEnd() => isRoundStarted = false;
+		public void OnRoundEnd() => _isRoundStarted = false;
 
-		public void OnContain106(ContainingEventArgs ev) => isContain106 = true;
+		public void OnContain106(ContainingEventArgs ev) => _isContain106 = true;
 
 		public void OnPlayerLeave(LeftEventArgs ev)
 		{
-			if (!isRoundStarted || ev.Player.Role == RoleType.Spectator || ev.Player.Position.y < -1997 || (ev.Player.CurrentRoom.Zone == ZoneType.LightContainment && Map.IsLCZDecontaminated)) return;
+			if (!_isRoundStarted || ev.Player.Role == RoleType.Spectator || ev.Player.Position.y < -1997 || (ev.Player.CurrentRoom.Zone == ZoneType.LightContainment && Map.IsLCZDecontaminated)) return;
 
-			bool is035 = false;
-			bool isSH = false;
-			if (isContain106 && ev.Player.Role == RoleType.Scp106) return;
-			Dictionary<Player, bool> spies = null;
+			var is035 = false;
+			var isSH = false;
+			
+			if (_isContain106 && ev.Player.Role == RoleType.Scp106) return;
 			var role = Loader.Plugins.FirstOrDefault(pl => pl.Name == "EasyEvents")?.Assembly.GetType("EasyEvents.Util")?.GetMethod("GetRole")?.Invoke(null, new object[] {ev.Player});
 			try
 			{
-				is035 = ev.Player.Id == TryGet035()?.Id;
+				is035 = Scp035.API.IsScp035(ev.Player);
 			}
 			catch (Exception x)
 			{
@@ -59,30 +43,21 @@ namespace DCReplace
 
 			try
 			{
-				isSH = TryGetSH().Contains(ev.Player);
+				isSH = SerpentsHand.API.IsSerpent(ev.Player);
 			}
 			catch (Exception x)
 			{
 				Log.Debug("Serpents Hand is not installed, skipping method call...");
 			}
 
-			try
-			{
-				spies = TryGetSpies();
-			}
-			catch (Exception x)
-			{
-				Log.Debug("CISpy is not installed, skipping method call...");
-			}
-			
-			Player player = Player.List.FirstOrDefault(x => x.Role == RoleType.Spectator && x.UserId != string.Empty && x.UserId != ev.Player.UserId && !x.IsOverwatchEnabled);
-			if (player != null)
+			var player = Player.List.FirstOrDefault(x => x.Role == RoleType.Spectator && x.UserId != string.Empty && x.UserId != ev.Player.UserId && !x.IsOverwatchEnabled);
+			if (player == null) return;
 			{
 				if (isSH)
 				{
 					try
 					{
-						TrySpawnSH(player);
+						SerpentsHand.API.SpawnPlayer(player);
 					}
 					catch (Exception x)
 					{
@@ -90,22 +65,11 @@ namespace DCReplace
 					}
 				}
 				else player.SetRole(ev.Player.Role);
-				if (spies != null && spies.ContainsKey(ev.Player))
-				{
-					try
-					{
-						TrySpawnSpy(player, ev.Player, spies);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("CISpy is not installed, skipping method call...");
-					}
-				}
 				if (is035)
 				{
 					try
 					{
-						TrySpawn035(player);
+						Scp035.API.Spawn035(player);
 					}
 					catch (Exception x)
 					{
@@ -114,12 +78,12 @@ namespace DCReplace
 				}
 
 				// save info
-				Vector3 pos = ev.Player.Position;
+				var pos = ev.Player.Position;
 				var inventory = ev.Player.Inventory.items.Select(x => x.id).ToList();
-				float health = ev.Player.Health;
-				uint ammo1 = ev.Player.Ammo[(int)AmmoType.Nato556];
-				uint ammo2 = ev.Player.Ammo[(int)AmmoType.Nato762];
-				uint ammo3 = ev.Player.Ammo[(int)AmmoType.Nato9];
+				var health = ev.Player.Health;
+				var ammo1 = ev.Player.Ammo[(int)AmmoType.Nato556];
+				var ammo2 = ev.Player.Ammo[(int)AmmoType.Nato762];
+				var ammo3 = ev.Player.Ammo[(int)AmmoType.Nato9];
 
 				Timing.CallDelayed(0.3f, () =>
 				{
